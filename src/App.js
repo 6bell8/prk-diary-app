f9f14e13bd09abe487c0a4dcddbf75db42d81f5b
flyingjjang051 <flyjjang051@gmail.com>
2022-09-06 14:51:09 +0900

    insert diary

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./css/layout.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import DiaryEditor from "./components/DiaryEditor";
 
 function App() {
+  const insertDiary = function (writer, contents, emotion) {
+    console.log("writer===", writer);
+    console.log("contents===", contents);
+    console.log("emotion===", emotion);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <DiaryEditor insertDiary={insertDiary} />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
